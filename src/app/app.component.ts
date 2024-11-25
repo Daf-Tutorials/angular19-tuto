19247a122000d2f1398cd06dcef0266ce7ccb34c
Daf <afahd.ali@gmail.com>
2024-11-25 15:45:13 +0100

    feat: implement house details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
   <main>
       <header class="brand-name">
-        <img class="brand-logo" src="/assets/angular.svg" alt="logo" aria-hidden="true" />
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet/>
       </section>
     </main>
   `,
